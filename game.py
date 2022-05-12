86fa383a46978f24d6a3837b255131ffb0bcd75f
elswindle <elswindle@gmail.com>
2022-05-12 09:28:55 -0700

    initial checkin

---
+++ game.py
@@ -0,0 +1,22 @@
+import csv as csv
+from factory import Factory
+from item import Item
+from recipe import Recipe
+
+class Game:
+    def __init__(self):
+        self.factory = 0
+        self.item_list = {}
+        self.recipe_list = {}
+
+    def loadItemList(self, path):
+        item_csv = csv.reader(open(path), delimiter=',')
+        next(item_csv)
+        for row in item_csv:
+            self.item_list[row[0]] = Item(row)
+
+    def printItemList(self):
+        for item in self.item_list.values():
+            print(item)
+
+    
