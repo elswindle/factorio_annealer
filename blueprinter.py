fa0f0004e9eff63d8e532b81c0fc0c441183746d
elswindle <elswindle@gmail.com>
2022-06-06 13:10:48 -0700

    more testing

--- blueprinter.py
+++ blueprinter.py
@@ -1,18 +1,25 @@
 import factory
 # from draftsman import Blueprint, BlueprintBook
-import draftsman.classes.blueprint
-import draftsman.blueprintable
+# from draftsman.classes.blueprint import Blueprint
+# from draftsman.classes.blueprintbook import BlueprintBook
+from draftsman.blueprintable import Blueprint
+from draftsman import utils
 
 class Blueprinter:
     def readBlueprint(self, path):
         file = open(path)
         # bp_str = file.readline()
         # bp_str.rstrip('\n')
-        bp_str = "0eJytldtugzAMht/F10kFAUrhcq8xVRMHq4sGgZFQrap495m2q6i2laT0ikPs/4ud/MkR8qrHtpPKQHoEWTRKQ/p6BC13KqvGf+bQIqQgDdbAQGX1+JVjRqEwMJCqxC9I/YHNJlVZPskQw5YBKiONxDNzEsagbTSNEIPUKJr7q4jBgdJWEWmUssPiPOxdVA5vqq9z7Ggu7ATWY2rbNWVPkXsK4DW9V8iDET7O97aY38zgwozmmWLC1C1i6Qzz7GHBUpiwZ4W2LG2y4oNLpbEzlPh/gd48NHLQdZBdT2SrZie1kQUv3lEb3uFnT8+7E+cWiPghhAth80jPR/vcCoe/hJMHmm6j63vTnvxl7h9vj4abNffT3H2tYW1BXexv4UJbbHAuHHaUb+3xu6tnczJHT1o8p/LWi8rjTrszfvbdY7VfNvcr3J6HR9z1tmewx06fBMXGD+NExKFIkiAhf1EfkK5xeLlGD8M3Wc6mIQ=="
+        bp_str = "0eNqdmt1u4jAQhd8l16Gyx7Ed8yqragU06kaiCQqh2qri3TcpPyplspyTS1ryceKZsY/H/szW20O16+qmz5afWb1pm322/PWZ7evXZrUd/9Z/7KpsmdV99ZblWbN6Gz91q3qbHfOsbl6qv9nSHp/zrGr6uq+r0/NfHz5+N4e3ddUNX7g+ue+HZ1//9IsvRJ7t2v3wVNuMPzWSUp59jMBjfgeRK2Rdvy6qbbXpu3qz2LXb6h4kcgYND1XDD67bQzdK8zEP7lmhO1Si+GmJBfmeToN4UokKCaQSr0EiqUSFlFfI5tC9Vy9TOsrTsJohYi91NwT3639eISby3aImyxpMlwRVV6EhLTleujAhXy+pFDafdQqb0FYtC8um9AQmgKl0rv1wGzLRkHCK27Mydwt1GrS8mSgXmz+rulmcp9R7cPF0RsuTv4VbDc5mv1XnBzFsSHQMnvJJHT+tvkWwODs9zlFDOiYkkiZDosVbilv4FLa8UP1Pqpaa4qksChw8UPB4gQ+5qqZBZJNSx1CFI8K9cmIzXtXoDKUxUhqdpeDhe1geLk9OGLjznHJ4vbEnagSY8OrjcCa8FHmcCZutiDPhiko4s4RjRAQJX4/wKBWGXZYRKL4LweNUsG7NqqavcDzmsTa4fgSPd4F7OSLegZ2bEWjkoRoGrhoh0iaxvgmAerhqHB5vD1eNw+Pt4apxeLw9XEMOD5TH1yAiUHANFUSg4BoqiEBFpulyEivmZ9NFcitWa7r4b4avXbe7tusVuReqBkjzfGzSfWww7DSsbpsD2wiYwAgyOuY/wxMcE75zXhDxC2x/QNSNfWD7AxOYMMs9i0Hcc4icwRX3uLpCyRlciJk4g4swo+EMLsS0VDNHisf78CicZ4ZkOtIzQ9CC9MwQ1JOeGYIG0jNDULZbIGrvKZY85rG2RHpmBFoa0jNDUHaRgaDCQzWMIz0zpK2g2oI/Z4ygIT1pwyGdgbThEDSSNhyClqQNh6CJtOEINBnShkNQS9pwCCqkDYegVPv60meWu7agVgOpYNeN+PiUIvlZvly87stTmNULBweAXo50jVzz2nIa2ea1rtEaw0548fHhijV2Vk8ce3VrZJ6t94itt8ZhhzfXfjiquqDOvZ3R4+VnHSWOQ4zEjb0lIBNH6ZE6S3cGOOY0JZvxE9rSrJO/+xH06ok/2zAQ/ZydvjowxWHb0U4/aaevD0xx6BsxTufQl2ImOGzGO30etWxveYrD7mGcnuWWXR8mOMLms9Pz8Nu9APyy1sBCb2tZYTcqo9Dn/HSPbfnt2luevVfd/uS8SlvEJNGHYUWzcjz+Az5/z8Y="
         print(bp_str)
-        bp = draftsman.blueprintable.get_blueprintable_from_string(bp_str)
+        bp = Blueprint(bp_str)
 
         print(bp)
+
+        for entity in bp.entities:
+            print(entity)
+        
+        print(123)
 
     def generateFactoryBlueprint(self, factory):
         print("hello")
